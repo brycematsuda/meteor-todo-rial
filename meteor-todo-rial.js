5bfdf495f8f004ec60da72bbb4859ed8984a3409
Bryce Matsuda <iexist4jcore@gmail.com>
2014-12-14 14:36:56 -1000

    Add extended M3U parsing! (WIP: playlist manager/checklist)

--- meteor-todo-rial.js
+++ meteor-todo-rial.js
@@ -4,7 +4,7 @@ if (Meteor.isClient) {
  // This code only runs on the client
  Template.body.helpers({
   tasks: function() {
-    return Tasks.find({},  {sort: {createdAt: -1}});
+    return Tasks.find({});
   }
 });
 
@@ -21,6 +21,33 @@ if (Meteor.isClient) {
 
     // Clear form
     event.target.text.value = "";
+
+    // Prevent default form submit
+    return false;
+  },
+  "submit .m3u-parse": function (event) {
+    
+    // Get textarea value, and filter out only the "#EXTINF" with song information.
+    var parseText = event.target.textarea.value.split("\n");
+
+    var songArray = jQuery.grep(parseText, function( n ) {
+      return (n.substr(0, 7) == "#EXTINF");
+    });
+
+    // Insert list of songs into task list.
+    var i = 0;
+    for (i = 0; i < songArray.length; i++){
+
+      var splitInf = songArray[i].split(",");
+      
+      Tasks.insert({
+        text: splitInf[1],
+        createdAt: new Date() // current time
+      });
+    }
+
+    // Clear form
+    event.target.textarea.value = "";
 
     // Prevent default form submit
     return false;
